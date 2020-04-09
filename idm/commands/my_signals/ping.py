e1f0b19dcefc3e21afeca60ae85cc3d0b5e9dd69
rvolov616 <63092676+rvolov616@users.noreply.github.com>
2020-04-09 19:46:20 +0600

    Update ping.py

--- idm/commands/my_signals/ping.py
+++ idm/commands/my_signals/ping.py
@@ -13,9 +13,9 @@ def ping(event: MySignalEvent) -> str:
 
     r_type = 'ПОНГ' if event.command == "пинг" else "ПАУ" if event.command == "пиу" else "КОНГ"
 
-    if delta > 15:r_type += "\nТРЕВОГА. ШИНАПРОВОД СГРЫЗЕН!!!!"
-    elif delta > 10:r_type += "\nТак кто-то конкретно грызет шинапровод."
-    elif delta > 5:r_type += "\nТак кто-то начинает грызть шинапровод."
+    if delta > 15:r_type += "\nХулинг"
+    elif delta > 10:r_type += "\nТак, что-то пинг большой"
+    elif delta > 5:r_type += "\nТак кто-то начинает грызть шинапровод"
     else:r_type += "\neee сытый, спасибо что накормили его."
 
     message = f"""{r_type}
